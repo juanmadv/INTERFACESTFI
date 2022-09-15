902842b3913d59dcb20709cd798f773cd63cf1c9
Juan Manuel Seoane <juanmaaa.s1988@gmail.com>
2022-09-15 18:11:20 -0300

    cambios

--- Interfaces/Login.cs
+++ Interfaces/Login.cs
@@ -1,10 +1,12 @@
-﻿using Security;
+﻿using BE;
+using Security;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -17,34 +19,90 @@ namespace Interfaces
         {
             InitializeComponent();
         }
+        private ProxyAuth auth = new ProxyAuth();
 
         private void Login_Load(object sender, EventArgs e)
         {
-            string check = null;
-            ProxyAuth auth = new ProxyAuth();
+            //string IP = Dns.GetHostEntry(Dns.GetHostName()).AddressList[1].ToString();
+            string IP = Dns.GetHostName();//.AddressList[1].ToString();
+            var a = new System.Net.IPHostEntry();
+           
+            //Dns.GetHostByName(Dns.GetHostName()).AddressList[0].ToString();
+
+           // MetroFramework.MetroMessageBox.Show(this, "IP Address is :\n" + IP, "IpObtenida", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            bool check = false;
+
+            
+
             try
             {
-                 check = auth.inicio();
+                
+                 check = auth.Inicio();
             }
-            catch (Exception)
+            catch (Exception e2)
             {
-                MetroFramework.MetroMessageBox.Show(this,"No se puede conectar con la base de datos, por favor actualice la conexion a continuacion...");                            
+                MetroFramework.MetroMessageBox.Show(this,"No se puede conectar con la BD," + e2.Message + e2.StackTrace, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);                            
             }
             
             
-            if (check != null)
-            {
+            if (check)
+            {   //se deja para probar cambiar la conexion
+               // Form formulario = new UIConexionBD();
+               // formulario.ShowDialog();
+
+
                 //logica login
             }
             else
             {
 
-                Form formulario = new UIConexionBD();
+                MetroFramework.Forms.MetroForm formulario = new UIConexionBD();
+                formulario.StyleManager = this.StyleMAIN;
                 formulario.ShowDialog ();
+                //this.Close();
             }
 
 
         
+        }
+
+        private void metroBtnLogin_Click(object sender, EventArgs e)
+        {
+            Patente pat = auth.Login(new User(metroTxtEmail.Text, metroTxtPass.Text),metroTxtPass.Text);
+            const string V = "Administracion";
+           const int i = 0;
+            if (pat != null)
+            {
+                switch (pat.Nombre)
+                {
+                    case "Administracion":
+                        new UIConexionBD().Show();
+                        break;
+                    default:
+                        break;
+                }
+
+
+
+            }
+            //ADMIN
+
+            if (1 == 1)
+            {
+                UIConexionBD fm = new UIConexionBD();
+                
+                fm.ShowDialog();
+                
+            }
+        }
+
+        private void metroCheckBox1_CheckStateChanged(object sender, EventArgs e)
+        {
+            //if (metroCheckBox1.Checked)
+            //    metroTxtPass.Enabled = false;
+            //else
+            //    metroTxtPass.Enabled = true;
+
         }
     }
 }
